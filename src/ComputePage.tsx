4f5de3dfbda3666e037bf46840d688163052c2b9
root <root@Vatsa>
2024-08-10 06:09:56 +0530

    Initial commit

--- src/ComputePage.tsx
+++ src/ComputePage.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import GenerateUserKey from './nillion/components/GenerateUserKey';
-import CreateClient from './nillion/components/CreateClient';
-import * as nillion from '@nillion/client-web';
+import React, { useEffect, useState } from "react";
+import GenerateUserKey from "./nillion/components/GenerateUserKey";
+import CreateClient from "./nillion/components/CreateClient";
+import * as nillion from "@nillion/client-web";
 
-import { NillionClient, NadaValues } from '@nillion/client-web';
-import StoreSecretForm from './nillion/components/StoreSecretForm';
-import StoreProgram from './nillion/components/StoreProgramForm';
-import ComputeForm from './nillion/components/ComputeForm';
-import ConnectionInfo from './nillion/components/ConnectionInfo';
+import { NillionClient, NadaValues } from "@nillion/client-web";
+import StoreSecretForm from "./nillion/components/StoreSecretForm";
+import StoreProgram from "./nillion/components/StoreProgramForm";
+import ComputeForm from "./nillion/components/ComputeForm";
+import ConnectionInfo from "./nillion/components/ConnectionInfo";
+import { Button } from "@mui/material";
 
 export default function Main() {
-  const programName = 'addition_simple';
-  const outputName = 'my_output';
-  const partyName = 'Party1';
+  const programName = "addition_simple";
+  const outputName = "my_output";
+  const partyName = "Party1";
   const [userkey, setUserKey] = useState<string | null>(null);
   const [client, setClient] = useState<NillionClient | null>(null);
   const [userId, setUserId] = useState<string | null>(null);
@@ -23,6 +24,8 @@ export default function Main() {
   const [additionalComputeValues, setAdditionalComputeValues] =
     useState<NadaValues | null>(null);
   const [computeResult, setComputeResult] = useState<string | null>(null);
+  const [stopWatch, setStopWatch] = useState<number | null>(null);
+  const [clickFlag, setClickFlag] = useState<boolean>(false);
 
   useEffect(() => {
     if (userkey && client) {
@@ -42,12 +45,12 @@ export default function Main() {
       </p>
       <ConnectionInfo client={client} userkey={userkey} />
 
-      <h1>1. Connect to Nillion Client {client && ' ✅'}</h1>
+      <h1>1. Connect to Nillion Client {client && " ✅"}</h1>
       <GenerateUserKey setUserKey={setUserKey} />
 
       {userkey && <CreateClient userKey={userkey} setClient={setClient} />}
       <br />
-      <h1>2. Store Program {programId && ' ✅'}</h1>
+      <h1>2. Store Program {programId && " ✅"}</h1>
       {client && (
         <>
           <StoreProgram
@@ -58,12 +61,12 @@ export default function Main() {
         </>
       )}
       <br />
-      <h1>3. Store Secrets {storeId_my_int1 && storeId_my_int2 && ' ✅'}</h1>
+      <h1>3. Store Secrets {storeId_my_int1 && storeId_my_int2 && " ✅"}</h1>
       {userId && programId && (
         <>
-          <h2>Store my_int1 {storeId_my_int1 && ' ✅'}</h2>
+          <h2>Store my_int1 {storeId_my_int1 && " ✅"}</h2>
           <StoreSecretForm
-            secretName={'my_int1'}
+            secretName={"my_int1"}
             onNewStoredSecret={(secret) => setStoreId_my_int1(secret.storeId)}
             nillionClient={client}
             secretType="SecretInteger"
@@ -74,9 +77,9 @@ export default function Main() {
             defaultProgramIdForComputePermissions={programId}
           />
 
-          <h2>Store my_int2 {storeId_my_int2 && ' ✅'}</h2>
+          <h2>Store my_int2 {storeId_my_int2 && " ✅"}</h2>
           <StoreSecretForm
-            secretName={'my_int2'}
+            secretName={"my_int2"}
             onNewStoredSecret={(secret) => setStoreId_my_int2(secret.storeId)}
             nillionClient={client}
             secretType="SecretInteger"
@@ -89,25 +92,80 @@ export default function Main() {
         </>
       )}
       <br />
-      <h1>4. Compute {computeResult && ' ✅'}</h1>
+      <h1>4. Compute {computeResult && " ✅"}</h1>
       {client &&
         programId &&
         storeId_my_int1 &&
         storeId_my_int2 &&
         partyId &&
         additionalComputeValues && (
-          <ComputeForm
-            nillionClient={client}
-            programId={programId}
-            additionalComputeValues={additionalComputeValues}
-            storeIds={[storeId_my_int1, storeId_my_int2]}
-            inputParties={[{ partyName, partyId }]}
-            outputParties={[{ partyName, partyId }]}
-            outputName={outputName}
-            onComputeProgram={(result) => setComputeResult(result.value)}
-          />
+          <>
+            <ComputeForm
+              nillionClient={client}
+              programId={programId}
+              additionalComputeValues={additionalComputeValues}
+              storeIds={[storeId_my_int1, storeId_my_int2]}
+              inputParties={[{ partyName, partyId }]}
+              outputParties={[{ partyName, partyId }]}
+              outputName={outputName}
+              onComputeProgram={(result) => setComputeResult(result.value)}
+            />
+            {computeResult && (
+              <div>
+                <Stopwatch />
+              </div>
+            )}
+          </>
         )}
       <br />
     </div>
   );
 }
+
+const Stopwatch = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  let timer: NodeJS.Timeout;
+  useEffect(() => {
+    if (isRunning) {
+      timer = setInterval(() => {
+        if (startTime !== null) {
+          setElapsedTime(Date.now() - startTime);
+        }
+      }, 100);
+    } else if (!isRunning && startTime !== null) {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning, startTime]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+    setStartTime(Date.now() - elapsedTime);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setStartTime(null);
+    setElapsedTime(0);
+  };
+
+  return (
+    <div>
+      <h2>Stopwatch</h2>
+      <div>{(elapsedTime / 1000).toFixed(1)}s</div>
+      <Button onClick={handleStart} disabled={isRunning}>
+        Start
+      </Button>
+      <Button onClick={handleStop} disabled={!isRunning}>
+        Stop
+      </Button>
+      <Button onClick={handleReset}>Reset</Button>
+    </div>
+  );
+};
